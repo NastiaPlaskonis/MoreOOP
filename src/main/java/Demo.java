403ed44b04e559891f215c60beb4c4ab7b7bbedd
NastiaPlaskonis <116552566+NastiaPlaskonis@users.noreply.github.com>
2023-10-06 15:51:39 +0300

    Uploading all files

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -1,3 +1,13 @@
-public class Demo {
+package lotr;
 
+public class Demo {
+    public static void main(String[] args) {
+        Character character1 = CharacterFactory.createCharacter();
+        Character character2 = CharacterFactory.createCharacter();
+
+        System.out.println("Character 1: " + character1);
+        System.out.println("Character 2: " + character2);
+
+        GameManager.fight(character1, character2);
+    }
 }
